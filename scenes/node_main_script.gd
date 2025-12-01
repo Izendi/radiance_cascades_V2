80c98c55e386036f43363099e371397c719d4637
Izendi <cjwinters1404@gmail.com>
2025-12-01 15:50:26 +0900

    added an additonal shader to the test shader pool

--- scenes/node_main_script.gd
+++ scenes/node_main_script.gd
@@ -16,3 +16,8 @@ func _on_button_test_shader_pressed():
 func _on_button_sdf_circle_shader_pressed():
 	fullScreenQuadTexture_1 = $SubViewport_2.get_texture()
 	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1)
+
+
+func _on_button_grid_display_shader_pressed():
+	fullScreenQuadTexture_1 = $SubViewport_3.get_texture()
+	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1)
