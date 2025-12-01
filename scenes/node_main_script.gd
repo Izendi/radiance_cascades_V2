bf61d432dacb51ccd4dbe701d161f46ce7d49401
Izendi <cjwinters1404@gmail.com>
2025-12-01 15:19:27 +0900

    basic setup

---
+++ scenes/node_main_script.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var fullScreenQuadTexture_1: Texture = $SubViewport_1.get_texture()
+#@onready var bool_displayShaderUpdated: bool = false
+
+func _ready():
+	
+	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1) 
+
+
+func _on_button_test_shader_pressed():
+	fullScreenQuadTexture_1 = $SubViewport_1.get_texture()
+	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1)
+
+
+func _on_button_sdf_circle_shader_pressed():
+	fullScreenQuadTexture_1 = $SubViewport_2.get_texture()
+	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1)
