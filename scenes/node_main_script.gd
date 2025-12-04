7b07cda61d2ab2608bbd8dced23090166ca3bbea
Izendi <cjwinters1404@gmail.com>
2025-12-04 16:37:24 +0900

    more shader example code

--- scenes/node_main_script.gd
+++ scenes/node_main_script.gd
@@ -3,10 +3,18 @@ extends Node2D
 @onready var fullScreenQuadTexture_1: Texture = $SubViewport_1.get_texture()
 #@onready var bool_displayShaderUpdated: bool = false
 
+var time_passed: float = 0.0
+
 func _ready():
 	
 	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1) 
 
+func _physics_process(delta):
+	
+	time_passed = time_passed + float(delta)
+	
+	$SubViewport_2/MeshInstance2D.material.set_shader_parameter("total_elapsed_time", time_passed)
+	
 
 func _on_button_test_shader_pressed():
 	fullScreenQuadTexture_1 = $SubViewport_1.get_texture()
