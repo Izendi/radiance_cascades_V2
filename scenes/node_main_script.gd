01bb6225f6cb3531f23a8f51084be49e9c75cacc
Izendi <cjwinters1404@gmail.com>
2025-12-05 11:57:40 +0900

    added more shader examples for sdf and sdf rotation

--- scenes/node_main_script.gd
+++ scenes/node_main_script.gd
@@ -10,11 +10,14 @@ func _ready():
 	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1) 
 
 func _physics_process(delta):
+	pass
 	
+
+func _process(delta):
 	time_passed = time_passed + float(delta)
 	
-	$SubViewport_2/MeshInstance2D.material.set_shader_parameter("total_elapsed_time", time_passed)
-	
+	#$SubViewport_2/MeshInstance2D.material.set_shader_parameter("total_elapsed_time", time_passed)
+	#$SubViewport_4/MeshInstance2D.material.set_shader_parameter("total_elapsed_time", time_passed)
 
 func _on_button_test_shader_pressed():
 	fullScreenQuadTexture_1 = $SubViewport_1.get_texture()
@@ -28,4 +31,9 @@ func _on_button_sdf_circle_shader_pressed():
 
 func _on_button_grid_display_shader_pressed():
 	fullScreenQuadTexture_1 = $SubViewport_3.get_texture()
+	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1)
+
+
+func _on_button_SDF_pressed():
+	fullScreenQuadTexture_1 = $SubViewport_4.get_texture()
 	$MeshInstance2D.material.set_shader_parameter("src_tex", fullScreenQuadTexture_1)
